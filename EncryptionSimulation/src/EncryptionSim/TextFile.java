eb10c25434b9147745926962d000c396fbc8b4e9
toosah00 <129904299+toosah00@users.noreply.github.com>
2024-03-09 11:40:33 -0600

    FInal Commit

--- EncryptionSimulation/src/EncryptionSim/TextFile.java
+++ EncryptionSimulation/src/EncryptionSim/TextFile.java
@@ -4,19 +4,10 @@ import java.io.*;
 import java.util.Scanner;
 
 public class TextFile {
-	private String name;
 	private String fillData;
 	
 	public TextFile() {
 		
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 
 	public String getFillData() {
@@ -27,13 +18,12 @@ public class TextFile {
 		this.fillData = fillData;
 	}
 
-	public void create(String name) throws IOException {
-		File file = new File(this.name + ".txt");
-		System.out.println(file.getAbsolutePath());
+	public void create() throws IOException {
+		File file = new File("Sentence.txt");
 	}
 
 	public void fill(String fillData) throws IOException {
-		FileOutputStream file = new FileOutputStream(this.name+ ".txt");
+		FileOutputStream file = new FileOutputStream("Sentence.txt");
 		PrintWriter outFile = new PrintWriter(file);
 		
 		outFile.println(this.fillData);
@@ -43,7 +33,7 @@ public class TextFile {
 	}
 	
 	public void display() throws IOException {
-		FileInputStream inStream = new FileInputStream(this.name + ".txt");
+		FileInputStream inStream = new FileInputStream("Sentence.txt");
 		Scanner file = new Scanner(inStream);
 		String data;
 
@@ -59,7 +49,7 @@ public class TextFile {
 	
 	public void encryption() throws IOException {
 		//opens this file name
-		FileInputStream inStream = new FileInputStream(this.name + ".txt");
+		FileInputStream inStream = new FileInputStream("Sentence.txt");
 		Scanner scannerIn = new Scanner(inStream);
 		
 		while(scannerIn.hasNext())
@@ -83,7 +73,7 @@ public class TextFile {
 		inStream.close();
 		
 		//opens this file name
-		FileOutputStream outStream = new FileOutputStream(this.name+ ".txt");
+		FileOutputStream outStream = new FileOutputStream("Sentence.txt");
 		PrintWriter printWriterOut = new PrintWriter(outStream);
 		
 		printWriterOut.println(this.fillData);
@@ -91,5 +81,40 @@ public class TextFile {
 		printWriterOut.close();
 		outStream.close();
 		
+	}
+	
+	public void decryption() throws IOException {
+		//opens this file name
+				FileInputStream inStream = new FileInputStream("Sentence.txt");
+				Scanner scannerIn = new Scanner(inStream);
+				
+				while(scannerIn.hasNext())
+				{
+					this.fillData=scannerIn.nextLine();
+				}
+				
+				//encryption of fill data
+				int asciiStr[] = new int[this.fillData.length()];
+				
+				for(int i =0;i<this.fillData.length();i++) {
+					asciiStr[i] = (this.fillData.charAt(i)) - 1;
+				}
+				this.fillData = "";
+				for(int i =0;i<asciiStr.length;i++) {
+					this.fillData = this.fillData + (char)asciiStr[i];
+				}
+				
+				//closes
+				scannerIn.close();
+				inStream.close();
+				
+				//opens this file name
+				FileOutputStream outStream = new FileOutputStream("Sentence.txt");
+				PrintWriter printWriterOut = new PrintWriter(outStream);
+				
+				printWriterOut.println(this.fillData);
+				
+				printWriterOut.close();
+				outStream.close();
 	}
 }
